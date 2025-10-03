3e71d4e96b625bc8f612c9b2dac598c020009ed9
Sahil Gulati <gulatisahil599@gmail.com>
2025-10-04 02:03:42 +0530

    third

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',           // Allow external connections (needed on Render)
-    port: process.env.PORT || 5173,  // Use Render’s PORT or fallback
-    strictPort: true            // Fail if port is in use
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173
+  build: {
+    outDir: 'dist' // production build folder
   }
 })
