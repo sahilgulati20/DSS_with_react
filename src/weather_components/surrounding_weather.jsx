14880a6b266557b72f1ba79fc8715d9eee768f03
Sahil Gulati <gulatisahil599@gmail.com>
2025-10-11 16:28:06 +0530

    update

--- src/weather_components/surrounding_weather.jsx
+++ src/weather_components/surrounding_weather.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // --- IMPORTANT ---
 // Replace "YOUR_OPENWEATHER_API_KEY" with your actual API key from OpenWeatherMap
-const API_KEY = "20743b1868d48b41d80c78d2f6c73391";
+const API_KEY = "123456789abcdef123456789abcdef";
 
 // Icon components for the new design
 const MapPinIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline-block mr-2" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 21l-4.95-6.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" /></svg>;
